86c36939b0fb62afa5af9e498d83e2886d012d66
abdulziz <abdullziz.hallak.1991@gmail.com>
2025-10-26 11:40:40 +0300

    26-10-2025

--- lib/app/app_button.dart
+++ lib/app/app_button.dart
@@ -1,7 +1,5 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../features/diseases_discovery/presentation/widgets/app_text_style.dart';
 
 class AppButton extends StatelessWidget {
